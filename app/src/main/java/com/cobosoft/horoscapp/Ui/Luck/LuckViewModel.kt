bc9759a464b41c4b0509ea7549748f25b92f6e17
Javicoso <javicoso810@gmail.com>
2023-12-13 17:07:38 +0100

    added CuartaParte

---
+++ app/src/main/java/com/cobosoft/horoscapp/Ui/Luck/LuckViewModel.kt
@@ -0,0 +1,11 @@
+package com.cobosoft.horoscapp.Ui.Luck
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+
+class LuckViewModel @Inject constructor(): ViewModel(){
+}
